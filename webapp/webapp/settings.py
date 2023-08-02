eeba342f4436797f97600930c081d5c351db3f23
Xerxez <info@xerxez.in>
2023-08-02 19:19:37 +0530

    Third Commit

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)b@5-bg$_!-nc@yn6(1l2k2*ib6kz+8#k7p@nt%5-l=fg6kb=t'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG'))=="1"
 
 ALLOWED_HOSTS = []
 
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'webapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -80,6 +81,28 @@ DATABASES = {
     }
 }
 
+DB_USERNAME=os.environ.get("POSTGRES_USER")
+DB_PASSWORD=os.environ.get("POSTGRES_PASSWORD")
+DB_DATABASE=os.environ.get("POSTGRES_DB")
+DB_HOST=os.environ.get("POSTGRES_HOST")
+DB_PORT=os.environ.get("POSTGRES_PORT")
+
+DB_IS_AVAIL = all ([DB_USERNAME,DB_PASSWORD,DB_DATABASE,DB_HOST,DB_PORT])
+POSTGRES_READY=str(os.environ.get('POSTGRES_READY')) == "1"
+
+if DB_IS_AVAIL and POSTGRES_READY:
+    DATABASES = {
+        "default":{
+            'ENGINE': 'django.db.backends.postgres',
+            'NAME': 'DB_USERNAME',
+            'USER':'DB_USERNAME',
+            'PASSWORD':'DB_PASSWORD',
+            'HOST':'DB_HOST',
+            'PORT':'DB_PORT'
+        }
+    }
+
+#print(DATABASES)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
